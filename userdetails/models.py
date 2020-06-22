0302c7134e059f020dc87e8c1bc5bff27f5bbdb0
shrin00 <47292303+shrin00@users.noreply.github.com>
2020-06-23 00:03:23 +0530

    register form

--- userdetails/models.py
+++ userdetails/models.py
@@ -3,11 +3,11 @@ from django.db import models
 # Create your models here
 
 class details(models.Model):
-    name=models.CharField(max_length=255)
-    email=models.CharField(max_length=255)
-    password=models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    first_name=models.CharField(max_length=255, blank=True, null=True)
+    last_name=models.CharField(max_length=255, blank=True, null=True)
     contact=models.IntegerField()
     gender=models.CharField(max_length=250, blank=True, null=True)
 
-    def __init__(self):
-        return name
+    def __str__(self):
+        return self.email
